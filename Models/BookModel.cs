a9b0fd118e8498c056a0bb226441364bf3e70d77
mariuszpaluch2001 <mariuszpaluch001@gmail.com>
2022-10-26 10:09:49 +0100

    Remove privacy page.

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -2,5 +2,13 @@
 {
     public class BookModel
     {
+        public int id { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+        public int date { get; set; }
+        public string publisher { get; set; }
+        public string user { get; set; }
+        public string reserved { get; set; }
+        public string leased { get; set; }
     }
 }

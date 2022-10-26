09b430766b2089658778cf65188d8e477fbb5dbb
mariuszpaluch2001 <mariuszpaluch001@gmail.com>
2022-10-26 15:16:07 +0100

    Add basic book create functionality.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,114 @@
+﻿using LibraryManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace LibraryManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        private static IList<BookModel> books = new List<BookModel>()
+        {
+            new BookModel(){ 
+                id = 1, 
+                author = "Autor",
+                title="title",
+                date=2022,
+                publisher="publisher",
+                user="",
+                reserved="2022-10-12",
+                leased =""
+            },
+            new BookModel(){
+                id = 2,
+                author = "Autor2",
+                title="title2",
+                date=2022,
+                publisher="publisher2",
+                user="",
+                reserved="2022-10-12",
+                leased =""
+            },
+            new BookModel(){
+                id = 3,
+                author = "Autor3",
+                title="title3",
+                date=2022,
+                publisher="publisher3",
+                user="",
+                reserved="2022-10-12",
+                leased =""
+            }
+        };
+        // GET: BookController
+        public ActionResult Index()
+        {
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View(new BookModel());
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookModel bookModel)
+        {
+            bookModel.id = books.Count + 1;
+                books.Add(bookModel);
+                return RedirectToAction(nameof(Index));
+
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,14 +1,23 @@
-﻿namespace LibraryManagement.Models
+﻿using System.ComponentModel;
+
+namespace LibraryManagement.Models
 {
     public class BookModel
     {
         public int id { get; set; }
+        [DisplayName("Autor")]
         public string author { get; set; }
+        [DisplayName("Tytuł")]
         public string title { get; set; }
+        [DisplayName("Data")]
         public int date { get; set; }
+        [DisplayName("Wydawca")]
         public string publisher { get; set; }
+        [DisplayName("Użytkownik")]
         public string user { get; set; }
+        [DisplayName("Data zarezerwowania")]
         public string reserved { get; set; }
+        [DisplayName("Data wypożyczenia")]
         public string leased { get; set; }
     }
 }
